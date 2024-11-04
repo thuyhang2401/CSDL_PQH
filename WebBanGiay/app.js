245ba29bb0905cf77901a404e261db4deeddc9dc
thuyhang2401 <truongthuyhanghhn@gmail.com>
2024-11-04 20:22:30 +0700

    Add connection string

--- WebBanGiay/app.js
+++ WebBanGiay/app.js
@@ -3,9 +3,21 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+
+// MongoDB connection string
+mongoose.connect('mongodb://localhost:27017/QuanLyBanGiay');
+
+// test connection
+const db = mongoose.connection
+
+db.on('error', console.error.bind(console, 'connection error:'))
+db.once('open', function () {
+  console.log('MongoDB connection successful')
+});
 
 var app = express();
 
@@ -24,12 +36,12 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
